9211a6a39bc7382404c4853ab5a4bccd500b273e
Jennifer Haddock <jentx123@hotmail.com>
2017-11-29 03:21:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
   fetch('https://api.github.com/repos/${repo}/forks', {
-  //  method: 'post',
+    method: 'post',
     headers: {
     Authorization: `token ${token}`
   }).then(res => res.json()).then(showResults(data))
